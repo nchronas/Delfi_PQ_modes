3d6b6174ad5a5cdc6fda223de4b5d00a1a311e1b
nchronas <nchronas@gmail.com>
2018-03-28 17:12:50 +0200

    Added modes and called Cthulhu

--- src/sat_modes.c
+++ src/sat_modes.c
@@ -8,12 +8,62 @@ static struct _sat_state sat_state = {
 
 void satellite_mode() {
 
+  bool transit = false;
+  sat_mode_id next_mode = ACTIVATION_MODE_ID;
+
+  if(sat_state.current_mode == ACTIVATION_MODE_ID) {
+
+    bool elapsed_time = 0;
+
+    elapsed_time = sat_elapsed_time();
+
+    transit = activation_mode(&next_mode,
+                              sat_submode_act_id state,
+                              sat_submode_act_id next_state,
+                              time_elapsed);
+
+  } else if(sat_state.current_mode == DEPLOYMENT_MODE_ID) {
+
+      bool elapsed_time = false;
+      bool deployed = false;
+
+      sat_mode_id next_mode = DEPLOYMENT_MODE_ID;
+
+      elapsed_time = sat_elapsed_time();
+      deployed = sat_deployed();
+
+      transit = deployment_mode(&next_mode,
+                                sat_submode_act_id state,
+                                sat_submode_act_id next_state,
+                                time_elapsed);
+
+  } else if(sat_state.current_mode == SAFE_MODE_ID) {
+
+      bool batt_charged = false;
+      sat_mode_id next_mode = SAFE_MODE_ID;
+
+      batt_charged = sat_batt_charged();
+
+      transit = safe_mode(&next_mode, batt_charged);
+  }
+
+  if(transit == true) {
+    change_mode(next_mode);
+  }
+
+}
+
+void change_mode(const sat_mode_id next_mode) {
+
+  if(transition_check_sat_modes(sat_state.current_mode, next_mode) == true) {
+         sat_state.previous_mode = sat_state.current_mode;
+         sat_state.current_mode = next_mode;
+  }
 }
 
 sat_mode_id get_satellite_mode() {
   return sat_state.current_mode;
 }
-
 
 bool transition_check_sat_modes(sat_mode_id current, sat_mode_id next) {
 
@@ -52,4 +102,143 @@ bool transition_check_sat_modes(sat_mode_id current, sat_mode_id next) {
   }
 
   return true;
+}
+
+bool sat_elapsed_time() {
+
+  bool elapsed = false;
+  uint32_t timer = 0;
+
+  get_parameter(EPS_ELAPSED_TIME_PAR_ID, );
+  if(timer > SAT_ACTIVATION_TIME) {
+    elapsed = true;
+  }
+
+  return elapsed;
+}
+
+bool sat_batt_ready() {
+
+  bool ready = false;
+  float volt = 0.0;
+  float threshold = 0.0;
+
+  get_parameter(EPS_BATT_VOLT_PAR_ID, );
+  get_parameter(EPS_BATT_VOLT_THRES_PAR_ID, );
+
+  if(volt > threshold) {
+    ready = true;
+  }
+
+  return ready;
+}
+
+bool sat_batt_charged() {
+
+  bool ready = false;
+  float volt = 0.0;
+  float threshold = 0.0;
+
+  get_parameter(EPS_BATT_VOLT_PAR_ID, );
+  get_parameter(EPS_BATT_SAFE_THRES_PAR_ID, );
+
+  if(volt > threshold) {
+    ready = true;
+  }
+
+  return ready;
+}
+
+bool activation_mode(sat_mode_id *next_mode,
+                     sat_submode_act_id state,
+                     sat_submode_act_id next_state,
+                     bool time_elapsed) {
+
+  bool transition = false;
+
+  if(!C_ASSERT(current < LAST_ACT_ID) == true) {
+    //error
+    return false;
+  }
+
+  if(state == ACTIVATED_ACT_ID) {
+
+    transition = true;
+    *next_mode = DEPLOYMENT_MODE_ID;
+
+  } else if(state == NOT_ACTIVATED_ACT_ID && time_elapsed == true) {
+
+    transition = true;
+    *next_mode = DEPLOYMENT_MODE_ID;
+
+  } else if(state == NOT_ACTIVATED_ACT_ID && time_elapsed == false) {
+
+    transition = false;
+    *next_mode = ACTIVATION_MODE_ID;
+
+  } else {
+    //error
+  }
+
+  return transition;
+}
+
+bool deployment_mode(sat_mode_id *next_mode,
+                     sat_submode_act_id state,
+                     sat_submode_act_id next_state,
+                     bool batt_ok,
+                     bool time_elapsed) {
+
+  bool transition = false;
+
+  if(!C_ASSERT(current < LAST_DEP_ID) == true) {
+    //error
+    return false;
+  }
+
+  if(state == DEPLOYED_DEP_ID) {
+
+    transition = true;
+    *next_mode = SAFE_MODE_ID;
+
+  } else if(state == NOT_DEPLOYED_DEP_ID && batt_ok == true) {
+
+    transition = false;
+
+  } else if(state == NOT_DEPLOYED_DEP_ID && batt_ok == true) {
+
+    transition = false;
+
+  } else if(state == WAIT_DEPLOYED_DEP_ID && time_elapsed == false) {
+
+    transition = false;
+
+  } else {
+    //error
+  }
+
+  return transition;
+}
+
+bool safe_mode(sat_mode_id *next_mode,
+               bool batt_charged) {
+
+  bool transition = false;
+
+  if(!C_ASSERT(current < LAST_ACT_ID) == true) {
+    //error
+    return false;
+  }
+
+  if(batt_charged == true) {
+
+    transition = true;
+    *next_mode = ADCS_MODE_ID;
+
+  } else {
+
+    transition = false;
+  }
+
+  return transition;
 }
--- src/sat_modes.h
+++ src/sat_modes.h
@@ -4,15 +4,22 @@
 #include <stdbool.h>
 
 typedef enum {
-    ACTIVATION_MODE_ID   = 0,
-    DEPLOYMENT_MODE_ID   = 1,
-    SAFE_MODE_ID         = 2,
-    ADCS_MODE_ID         = 3,
-    NOMINAL_MODE_ID      = 4,
-    OFF_MODE_ID          = 5,
-    LAST_MODE_ID         = 6,
-    JOHN_MODE_ID         = 7
+    DEBUG_MODE_ID        = 0,
+    ACTIVATION_MODE_ID   = 1,
+    DEPLOYMENT_MODE_ID   = 2,
+    SAFE_MODE_ID         = 3,
+    ADCS_MODE_ID         = 4,
+    NOMINAL_MODE_ID      = 5,
+    OFF_MODE_ID          = 6,
+    LAST_MODE_ID         = 7,
+    JOHN_MODE_ID         = 8
 }sat_mode_id;
+
+typedef enum {
+    ACTIVATED_ACT_ID      = 0,
+    NOT_ACTIVATED_ACT_ID  = 1,
+    LAST_ACT_ID           = 2
+}sat_submode_act_id;
 
 struct _sat_state {
     sat_mode_id current_mode;
@@ -24,5 +31,7 @@ void satellite_mode();
 sat_mode_id get_satellite_mode();
 
 bool transition_check_sat_modes(sat_mode_id current, sat_mode_id next);
+
+bool activation_mode(sat_mode_id *next_mode);
 
 #endif // _SAT_MODES_H
